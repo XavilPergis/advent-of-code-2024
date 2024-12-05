0a6f677b89f43c7bebbf1fde28a170cf04ffb514
Rigel <4951693+XavilPergis@users.noreply.github.com>
2024-12-05 06:49:48 -0500

    day5/part2

--- src/day5.rs
+++ src/day5.rs
@@ -4,14 +4,15 @@ use crate::{RunContext, RunnerRepository};
 
 pub fn add_variants(repo: &mut RunnerRepository) {
     repo.add_variant("part1", part1);
-    // repo.add_variant("part2", part2);
+    repo.add_variant("part2", part2);
 }
 
-// terrible memory layout lol
-fn parse(ctx: &mut RunContext) -> eyre::Result<(HashMap<u32, Vec<u32>>, Vec<Vec<u32>>)> {
+fn part1(ctx: &mut RunContext) -> eyre::Result<()> {
+    // A|B -> for each number N in update, find rules like A|N and check if A was already seen (how to discard non-matching rules?)
+
     let mut lines_iter = ctx.input.lines();
 
-    let mut implication_map = HashMap::<u32, Vec<u32>>::new();
+    let mut implications = HashMap::<u32, Vec<u32>>::new();
     let mut updates = vec![];
     while let Some(line) = lines_iter.next() {
         if line.is_empty() {
@@ -22,7 +23,7 @@ fn parse(ctx: &mut RunContext) -> eyre::Result<(HashMap<u32, Vec<u32>>, Vec<Vec<
             eyre::bail!("invalid dependency rule");
         };
         let (l, r) = (l.parse()?, r.parse()?);
-        implication_map.entry(r).or_default().push(l);
+        implications.entry(r).or_default().push(l);
     }
 
     while let Some(line) = lines_iter.next() {
@@ -34,14 +35,6 @@ fn parse(ctx: &mut RunContext) -> eyre::Result<(HashMap<u32, Vec<u32>>, Vec<Vec<
     }
 
     ctx.mark_parse_complete();
-
-    Ok((implication_map, updates))
-}
-
-fn part1(ctx: &mut RunContext) -> eyre::Result<()> {
-    // A|B -> for each number N in update, find rules like A|N and check if A was already seen (how to discard non-matching rules?)
-
-    let (implications, updates) = parse(ctx)?;
 
     let mut sum = 0;
     'outer: for update in &updates {
@@ -61,6 +54,89 @@ fn part1(ctx: &mut RunContext) -> eyre::Result<()> {
         }
 
         sum += update[(update.len() - 1) / 2];
+    }
+
+    println!("{sum}");
+
+    Ok(())
+}
+
+fn toposort(
+    out: &mut Vec<u32>,
+    seen: &mut HashSet<u32>,
+    current: u32,
+    valid: &HashSet<u32>,
+    graph: &HashMap<u32, Vec<u32>>,
+) {
+    if !seen.insert(current) {
+        return;
+    }
+    for &outgoing in graph[&current].iter().filter(|elem| valid.contains(elem)) {
+        toposort(out, seen, outgoing, valid, graph);
+    }
+    out.push(current);
+}
+
+fn part2(ctx: &mut RunContext) -> eyre::Result<()> {
+    let mut lines_iter = ctx.input.lines();
+
+    let mut ordered_after = HashMap::<u32, Vec<u32>>::new();
+    let mut ordered_before = HashMap::<u32, Vec<u32>>::new();
+    let mut updates = vec![];
+    while let Some(line) = lines_iter.next() {
+        if line.is_empty() {
+            break;
+        }
+
+        let Some((l, r)) = line.split_once('|') else {
+            eyre::bail!("invalid dependency rule");
+        };
+        let (l, r) = (l.parse()?, r.parse()?);
+        ordered_after.entry(r).or_default().push(l);
+        ordered_before.entry(l).or_default().push(r);
+    }
+
+    while let Some(line) = lines_iter.next() {
+        updates.push(
+            line.split(',')
+                .map(str::parse)
+                .collect::<Result<Vec<_>, _>>()?,
+        );
+    }
+
+    ctx.mark_parse_complete();
+
+    let mut sum = 0;
+    'outer: for update in &updates {
+        let valid_elems = update.iter().copied().collect::<HashSet<_>>();
+        let mut seen_elems = HashSet::<u32>::new();
+        for &num in update {
+            for implicated in ordered_after[&num]
+                .iter()
+                .filter(|elem| valid_elems.contains(elem))
+            {
+                if !seen_elems.contains(implicated) {
+                    // find number in update that is not ordered before anything, this is our root. (indegree=0)
+                    let root = update.iter().find(|elem| {
+                        ordered_before[&elem]
+                            .iter()
+                            .filter(|elem2| valid_elems.contains(elem2))
+                            .next()
+                            .is_none()
+                    }).unwrap();
+
+                    let mut out = Vec::new();
+                    let mut seen = HashSet::new();
+                    toposort(&mut out, &mut seen, *root, &valid_elems, &ordered_after);
+
+                    sum += out[(out.len() - 1) / 2];
+
+                    continue 'outer;
+                    // invalid!
+                }
+            }
+            seen_elems.insert(num);
+        }
     }
 
     println!("{sum}");
