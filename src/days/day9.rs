316ab99f5983b2c2aac37214506922cfa7ffa8ab
Rigel <4951693+XavilPergis@users.noreply.github.com>
2024-12-09 21:00:15 -0500

    day9/part1: cleanup + arithmetic sequence formula instead of loop

--- src/days/day9.rs
+++ src/days/day9.rs
@@ -14,10 +14,8 @@ fn part1(ctx: &mut RunContext) -> eyre::Result<u64> {
     let mut id = 0;
     for ch in input {
         let num = (ch - b'0') as usize;
-        if is_space {
-            if num != 0 {
-                stack.push_back((0, num));
-            }
+        if is_space && num != 0 {
+            stack.push_back((0, num));
         } else {
             id += 1;
             stack.push_back((id, num));
@@ -30,42 +28,34 @@ fn part1(ctx: &mut RunContext) -> eyre::Result<u64> {
     while !stack.is_empty() {
         let (id, mut len) = stack.pop_front().unwrap();
         if id == 0 {
-            if stack.is_empty() {
-                break;
-            }
+            // if stack.is_empty() || len == 0 {
+            //     break;
+            // }
             loop {
-                if len == 0 {
-                    break;
-                }
-                let (mut id2, mut len2) = stack.pop_back().unwrap();
-                while id2 == 0 {
-                    (id2, len2) = stack.pop_back().unwrap();
+                let (mut id, mut len2) = stack.pop_back().unwrap();
+                while id == 0 {
+                    (id, len2) = stack.pop_back().unwrap();
                 }
                 if len >= len2 {
                     len -= len2;
-                    for i in pos..pos + len2 {
-                        sum += (id2 - 1) * i;
-                    }
-                    // sum += (id2 - 1) * (len2 * pos + len2 * len2);
+                    sum += (id - 1) * len2 * (2 * pos + len2 - 1);
                     pos += len2;
                 } else {
-                    // sum += (id2 - 1) * (len * pos + len * len);
-                    for i in pos..pos + len {
-                        sum += (id2 - 1) * i;
-                    }
+                    sum += (id - 1) * len * (2 * pos + len - 1);
                     pos += len;
-                    stack.push_back((id2, len2 - len));
-                    len = 0;
+                    stack.push_back((id, len2 - len));
+                    break;
                 }
             }
         } else {
-            for i in pos..pos + len {
-                sum += (id - 1) * i;
-            }
-            // sum += (id - 1) * (len * pos + len * len);
+            sum += (id - 1) * len * (2 * pos + len - 1);
             pos += len;
         }
     }
+
+    // NOTE: the arithmetic sequence formula has a 0.5 factor, so we sum up twice the value for each
+    // entry and divide it out here. prolly doesnt actually matter that much for perf lol.
+    sum /= 2;
 
     Ok(sum as u64)
 }
