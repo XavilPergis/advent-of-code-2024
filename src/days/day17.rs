8d54f86ac8e6c41fe47aa7a2637c2628ac9d182b
Rigel <4951693+XavilPergis@users.noreply.github.com>
2024-12-17 04:51:39 -0500

    day17/part2

--- src/days/day17.rs
+++ src/days/day17.rs
@@ -2,7 +2,7 @@ use crate::{RunContext, RunnerRepository};
 
 pub fn add_variants(repo: &mut RunnerRepository) {
     repo.add_variant("part1", part1);
-    // repo.add_variant("part2", part2);
+    repo.add_variant("part2", part2);
 }
 
 fn parse_number(input: &[u8], cur: &mut usize) -> u64 {
@@ -37,6 +37,17 @@ struct State {
     output: Vec<u8>,
 }
 
+impl Default for State {
+    fn default() -> Self {
+        Self {
+            ip: 0,
+            registers: [0; 3],
+            program: vec![],
+            output: vec![],
+        }
+    }
+}
+
 // The value of a combo operand can be found as follows:
 // - Combo operands 0 through 3 represent literal values 0 through 3.
 // - Combo operand 4 represents the value of register A.
@@ -63,19 +74,13 @@ fn step_program(state: &mut State) {
         // (So, an operand of 2 would divide A by 4 (2^2); an operand of 5 would divide A by 2^B.)
         // The result of the division operation is truncated to an integer and then written to the A register.
         OP_ADV => {
-            let num = state.registers[REG_A];
-            let den = 1 << combo_op(&state, operand);
-            state.registers[REG_A] = num / den;
+            state.registers[REG_A] = state.registers[REG_A] >> combo_op(&state, operand);
         }
         OP_BDV => {
-            let num = state.registers[REG_A];
-            let den = 1 << combo_op(&state, operand);
-            state.registers[REG_B] = num / den;
+            state.registers[REG_B] = state.registers[REG_A] >> combo_op(&state, operand);
         }
         OP_CDV => {
-            let num = state.registers[REG_A];
-            let den = 1 << combo_op(&state, operand);
-            state.registers[REG_C] = num / den;
+            state.registers[REG_C] = state.registers[REG_A] >> combo_op(&state, operand);
         }
         // The bxl instruction (opcode 1) calculates the bitwise XOR of register B
         // and the instruction's literal operand, then stores the result in register B.
@@ -113,28 +118,29 @@ fn step_program(state: &mut State) {
     state.ip += 2;
 }
 
-fn part1(ctx: &mut RunContext) -> eyre::Result<u64> {
-    let mut state = State {
-        ip: 0,
-        registers: [0, 0, 0],
-        program: vec![],
-        output: vec![],
-    };
+fn parse_state(input: &[u8], state: &mut State) {
+    state.program.clear();
+    state.output.clear();
 
     let mut cur = 0;
     cur += 12; // "Register A: "
-    state.registers[REG_A] = parse_number(&ctx.input_scratch, &mut cur);
+    state.registers[REG_A] = parse_number(input, &mut cur);
     cur += 13; // "\nRegister B: "
-    state.registers[REG_B] = parse_number(&ctx.input_scratch, &mut cur);
+    state.registers[REG_B] = parse_number(input, &mut cur);
     cur += 13; // "\nRegister C: "
-    state.registers[REG_C] = parse_number(&ctx.input_scratch, &mut cur);
+    state.registers[REG_C] = parse_number(input, &mut cur);
     cur += 11; // "\n\nProgram: "
 
-    while cur < ctx.input_scratch.len() {
-        state.program.push(ctx.input_scratch[cur + 0] - b'0');
-        state.program.push(ctx.input_scratch[cur + 2] - b'0');
+    while cur < input.len() {
+        state.program.push(input[cur + 0] - b'0');
+        state.program.push(input[cur + 2] - b'0');
         cur += 4;
     }
+}
+
+fn part1(ctx: &mut RunContext) -> eyre::Result<u64> {
+    let mut state = State::default();
+    parse_state(&ctx.input_scratch, &mut state);
 
     while state.ip < state.program.len() {
         step_program(&mut state);
@@ -151,4 +157,75 @@ fn part1(ctx: &mut RunContext) -> eyre::Result<u64> {
     println!("{display}");
 
     Ok(0)
+}
+
+fn do_cycle(program: &[u8], a: u64) -> u8 {
+    let mut reg = [a, 0, 0];
+
+    fn combo_op(reg: &[u64; 3], value: u8) -> u64 {
+        match value {
+            n @ 0..=3 => n as u64,
+            n @ 4..=6 => reg[n as usize - 4],
+            n => panic!("invalid combo operand: {n}"),
+        }
+    }
+
+    for i in 0..program.len() / 2 {
+        let opcode = program[i * 2];
+        let operand = program[i * 2 + 1];
+        match opcode {
+            OP_ADV => reg[REG_A] = reg[REG_A] >> combo_op(&reg, operand),
+            OP_BDV => reg[REG_B] = reg[REG_A] >> combo_op(&reg, operand),
+            OP_CDV => reg[REG_C] = reg[REG_A] >> combo_op(&reg, operand),
+            OP_BXL => reg[REG_B] ^= operand as u64,
+            OP_BST => reg[REG_B] = combo_op(&reg, operand) & 7,
+            OP_BXC => reg[REG_B] ^= reg[REG_C],
+            OP_OUT | OP_JNZ => {}
+            op => panic!("unknown opcode: {op}"),
+        }
+    }
+
+    (reg[REG_B] & 7) as u8
+}
+
+// the last 7 bits of A are the only bits that contribute to the output for that particular cycle.
+// out[0] = op(A0)
+// A1 = A0 >> 3
+// out[1] = op(A1)
+// A2 = A1 >> 3
+// ...
+// A_n = A_(n-1) >> 3
+// out_n = op(A_n)
+fn test_candidate(program: &[u8], n: usize, cur_a: u64) -> Option<u64> {
+    for i in 0..(1 << 3) {
+        let prev_a = cur_a << 3 | i;
+        // let (_, value) = do_cycle(prev_a);
+        let value = do_cycle(program, prev_a);
+        if program[n] == value {
+            if n == 0 {
+                return Some(prev_a);
+            }
+            if let Some(value) = test_candidate(program, n - 1, prev_a) {
+                return Some(value);
+            }
+        }
+    }
+    // failed, backtrack.
+    None
+}
+
+fn part2(ctx: &mut RunContext) -> eyre::Result<u64> {
+    let mut state = State::default();
+    parse_state(&ctx.input_scratch, &mut state);
+
+    let target = state.program.clone();
+    let res = test_candidate(&state.program, target.len() - 1, 0).unwrap();
+
+    // state.registers[REG_A] = res;
+    // while state.ip < state.program.len() {
+    //     step_program(&mut state);
+    // }
+    // assert_eq!(&target, &state.output);
+
+    Ok(res as u64)
 }
